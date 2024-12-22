d19535334407d85abe8274c70f542b11404fab4d
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2024-12-22 22:42:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,39 +13,39 @@ def is_enabled(value, default):
         return default
 
 #main variables
-API_ID = int(environ.get('API_ID', '23475322'))
-API_HASH = environ.get('API_HASH', 'e00e5cebf073df8baba7db34ea0ebdc9')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '22475424'))
+API_HASH = environ.get('API_HASH', '5971f827128727176690c7f302ea46e5')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7365081445:AAHpw3v2DHHNPs5ZXtnzkTlh7mgd6g9tvMU')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6170050819').split()]
-USERNAME = environ.get('USERNAME', "https://telegram.me/Spark_Developer")
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002246184289'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Moviez_Gang')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002172731336').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6795403349').split()]
+USERNAME = environ.get('USERNAME', "https://telegram.me/Harshu801049")
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002266630227'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+L8FEK560H-Q5ZGQ1')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002477003775').split()]
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ytwithsg:I2CwsbDKuOTRhWfD@cluster0.rx9aw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002161681487'))
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/2d030f17278768080c72f.jpg')
-START_IMG = environ.get('START_IMG', 'https://graph.org/file/2d030f17278768080c72f.jpg')
-BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002208244566'))
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002214414905'))
-URL = environ.get('URL', '')
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002250942610'))
+QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/ccb9db43e62a2e524928e.jpg')
+START_IMG = environ.get('START_IMG', 'https://graph.org/file/4dad0cc16f190468454ee.jpg')
+BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002466854675'))
+DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002409484751'))
+URL = environ.get('URL', 'mytestbot-jvdfhbj.com')
 STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME').split()
 FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '600'))
 IS_VERIFY = is_enabled('IS_VERIFY', True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002161681487'))
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_To_Verify_By_Token/4")
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002250942610'))
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/Movie_Url_link_downloader/17")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "b912a60a94c5ba69a3eb7883802eca30de44f6d2")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'modijiurl.com')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "80118c72026ffc6debd8d4d909af5bf753a86f4a")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'adrinolinks.in')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "0c59cf71f13ad1735f9ce119010b5ca9f1f6835f")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'crlinks.in')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "180"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "180"))
+SHORTENER_API = environ.get("SHORTENER_API", "7ea6e07cfdac0e6410787c3d76670f19e67459bf")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'mdiskshortner.link')
+SHORTENER_API2 = environ.get("SHORTENER_API2", "7ea6e07cfdac0e6410787c3d76670f19e67459bf")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'mdiskshortner.link')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "7ea6e07cfdac0e6410787c3d76670f19e67459bf")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'mdiskshortner.link')
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
 
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
 QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
@@ -53,23 +53,23 @@ YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
 SEASONS = [f'season {i}'for i in range (1 , 23)]
 REF_PREMIUM = 30
 PREMIUM_POINT = 1500
-auth_channel = environ.get('AUTH_CHANNEL', '-1002061083620')
+auth_channel = environ.get('AUTH_CHANNEL', '-1002265269017')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002061083620'))
-request_channel = environ.get('REQUEST_CHANNEL', '-1002161681487')
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002446982479'))
+request_channel = environ.get('REQUEST_CHANNEL', '-1002323896727')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
-UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002236108663'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002236108663'))
+UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002353092442'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002423995393'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
-IMDB = is_enabled('IMDB', True)
+IMDB = is_enabled('IMDB', False)
 FILE_CAPTION = environ.get('FILE_CAPTION', f'{script.FILE_CAPTION}')
 IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
-LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', True)
+LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
 SPELL_CHECK = is_enabled('SPELL_CHECK', True)
 LINK_MODE = is_enabled('LINK_MODE', True)
